e4f4c1f0be936f518d9005175c29d0e1aa4204e5
Wangtao Zhou <anthonychou97@hotmail.com>
2026-05-17 22:43:06 +0800

    Add files via upload

--- auto_renew.sh
+++ auto_renew.sh
@@ -2,7 +2,7 @@ trap exit TERM
 certbot reconfigure --webroot -w /acme -v -n --cert-name my_site
 while :
 do
-  certbot renew
+  certbot renew --cert-name my_site
   echo "sleeping..."
   sleep 24h & wait ${!}
 done
